20058fc9e8a99444e9cc9f19f11de895baa364fe
SB <sbraun@itba.edu.ar>
2022-10-22 16:40:36 -0300

    Se actualizaron archivos de manejo de excepciones.

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,12 +1,22 @@
 #define ZERO_EXCEPTION_ID 0
+#define INVALID_OPCODE_EXCEPTION_ID 6
 
 static void zero_division();
+static void invalid_opcode();
 
 void exceptionDispatcher(int exception) {
 	if (exception == ZERO_EXCEPTION_ID)
 		zero_division();
+	if(exception == INVALID_OPCODE_EXCEPTION_ID)
+		invalid_opcode();
 }
 
 static void zero_division() {
-	// Handler para manejar excepcíon
+	// Imprimir por pantalla mensaje de error.
+	// Devolver a windows powershell.
+}
+
+static void invalid_opcode() {
+	// Imprimir por pantalla mensaje de error.
+	// Devolver a windows powershell.
 }
