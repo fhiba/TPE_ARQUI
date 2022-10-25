7860a2ded7c2f1600f7988a2ab3f6ce1b1233848
SB <sbraun@itba.edu.ar>
2022-10-25 14:44:48 -0300

    Se agrego la libreria de syscalls al Userspace con su .h

--- Userland/SampleCodeModule/c_lib/syscallslib.asm
+++ Userland/SampleCodeModule/c_lib/syscallslib.asm
@@ -1,13 +1,67 @@
 GLOBAL sys_write
-
 GLOBAL sys_read
+GLOBAL sys_printMem
+GLOBAL sys_clear
+GLOBAL sys_date
+GLOBAL sys_sleep
+GLOBAl sys_setScreen
+GLOBAL sys_inforeg
+GLOBAL sys_getLast
+GLOBAL sys_snapshotRegs
+GLOBAL divzero
+GLOBAL opcode
 
 sys_write:
-    mov rax,2
+    mov rax, 2
     int 80h
     ret
 
 sys_read:
-    mov rax,1
+    mov rax, 1
     int 80h
+    ret
+
+sys_printMem:
+    mov rax, 4
+    int 80h
+    ret
+
+sys_clear:
+    mov rax, 3
+    int 80h
+    ret
+
+sys_date:
+    mov rax, 6
+    int 80h
+    ret
+
+sys_sleep:
+    mov rax, 7
+    int 80h
+    ret
+
+sys_setScreen:
+    mov rax, 8
+    int 80h
+    ret
+
+sys_inforeg:
+    mov rax, 5
+    int 80h
+    ret
+
+sys_snapshotRegs:
+    mov rax, 12
+    int 80h
+    ret
+
+divzero:
+    mov rax, 4
+    xor rbx, rbx
+    div rbx
+    ret
+
+opcode:
+    ud2
     ret
--- Userland/SampleCodeModule/include/syscallslib.h
+++ Userland/SampleCodeModule/include/syscallslib.h
@@ -1,2 +1,18 @@
+#ifndef _syscallslib_
+#define _syscallslib_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
 int sys_read(int fd,char * buffer, size_t count);
 void sys_write(int fd, const char * buffer, size_t count);
+void sys_printMem(uint64_t, unsigned char *);
+void sys_clear();
+char sys_date(char value);
+void sys_sleep(int secs);
+void sys_setScreen(int mode);
+void sys_inforeg(registersStruct*);
+void sys_snapshotRegs(void);
+
+#endif
